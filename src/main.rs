1ab8b9cbe64af19857ddf16b1b18ef5fc1d554b0
Johannes Holland <joh.ho@gmx.de>
2024-01-01 12:46:04 +0100

    ui: add nannou sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use nannou::prelude::*;
+
+struct Model {}
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model).event(event).simple_window(view).run();
 }
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {}
